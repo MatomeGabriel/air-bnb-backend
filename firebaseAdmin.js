54ccb4796628d081b5d4501ff5d81b5945cd0156
Gabriel <montjagabriel@gmail.com>
2025-10-07 18:59:05 +0200

    edit firebaseAmin

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,3 +1,22 @@
+/**
+ * Firebase Admin SDK Initialization
+ *
+ * This module sets up and exports the Firebase Admin instance and storage bucket for use in the backend.
+ * - Loads service account credentials from environment variable FIREBASE_ADMIN_KEY
+ * - Initializes Firebase Admin with credentials and storage bucket
+ * - Exports both the admin instance and the storage bucket
+ *
+ * Usage:
+ *   const { admin, bucket } = require('./firebaseAdmin');
+ *
+ * Environment Variables:
+ *   FIREBASE_ADMIN_KEY: JSON string of Firebase service account credentials
+ *
+ * Exports:
+ *   admin  - The initialized Firebase Admin SDK instance
+ *   bucket - The Firebase Storage bucket instance
+ */
+
 require('dotenv').config();
 const admin = require('firebase-admin');
 
